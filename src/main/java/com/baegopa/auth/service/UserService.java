8f4b9a75990f52f063139f2c7a6e547b5364f2b5
Sehwan Kim <skim451@illinois.edu>
2017-07-08 19:58:44 +0900

    CRUD

--- src/main/java/com/baegopa/auth/service/UserService.java
+++ src/main/java/com/baegopa/auth/service/UserService.java
@@ -4,6 +4,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public interface UserService {
-	public List<HashMap> selectUserList() throws Exception;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.baegopa.auth.dao.UserDAO;
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+	
+	@Resource(name="userDAO")
+	private UserDAO userDAO; 
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List<HashMap> selectUserList() throws Exception {
+		return userDAO.selectUserList();
+	}
+	
+	
 }
