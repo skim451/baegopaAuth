06fe0b423bb14b979a71c073d7d914ed8ba175b3
Sehwan Kim <skim451@illinois.edu>
2017-07-25 15:06:24 +0900

    simple addition
    .

--- src/main/java/com/baegopa/auth/common/InvalidTokenException.java
+++ src/main/java/com/baegopa/auth/common/InvalidTokenException.java
@@ -2,6 +2,11 @@ package com.baegopa.auth.common;
 
 public class InvalidTokenException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String getMessage() {
 		return "Invalid Token Exception.";
